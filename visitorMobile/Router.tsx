4dbbea729d149d251aaa3f19f415f0b83539f3bd
JosefineM2000 <josefine.mende@epitech.eu>
2024-01-25 20:32:23 +0700

    [FE] [ADD] profile stack

--- visitorMobile/Router.tsx
+++ visitorMobile/Router.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -6,9 +6,61 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import RestaurantScreen from './src/pages/RestaurantScreen/RestaurantScreen';
 import AboutUsScreen from './src/pages/AboutUs/AboutUs';
 import ContactUsScreen from './src/pages/ContactUs/ContactUs';
-import MyProfileScreen from './src/pages/Profile/MyProfile';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Register from "./src/pages/ProfileScreen/Register/Register";
+import Profile from "./src/pages/ProfileScreen/Profile/Profile";
+import LoginScreen from "./src/pages/ProfileScreen/Login/Login";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {checkIfTokenIsValid} from "./src/services/userCalls";
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const ProfileStack = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const setLoggedInStatus = (status) => {
+    setLoggedIn(status);
+  };
+
+  const checkAuthentication = async () => {
+    const userToken = await AsyncStorage.getItem('userToken');
+    if (userToken) {
+      setLoggedIn(true);
+    } else {
+      const isUserTokenValid = await checkIfTokenIsValid({key: userToken});
+
+      if (isUserTokenValid === 'OK') {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+        await AsyncStorage.removeItem('userToken');
+      }
+      setLoggedIn(false);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthentication();
+  }, []);
+
+  return (
+    <Stack.Navigator>
+      {loggedIn ? (
+        <Stack.Screen name="My Profile">
+          {(props) => <Profile {...props} setLoggedInStatus={setLoggedInStatus} />}
+        </Stack.Screen>
+      ) : (
+        <>
+          <Stack.Screen name="Login">
+            {(props) => <LoginScreen {...props} setLoggedInStatus={setLoggedInStatus} />}
+          </Stack.Screen>
+          <Stack.Screen name="Register" component={Register} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+};
 
 const MyTabs = () => {
   return (
@@ -24,7 +76,7 @@ const MyTabs = () => {
               iconName = focused ? 'information-circle' : 'information-circle-outline';
             } else if (route.name === 'ContactUs') {
               iconName = focused ? 'call' : 'call-outline';
-            } else if (route.name === 'MyProfile') {
+            } else if (route.name === 'Profile') {
               iconName = focused ? 'person' : 'person-outline';
             }
 
@@ -37,7 +89,7 @@ const MyTabs = () => {
         <Tab.Screen name="RestaurantScreen" component={RestaurantScreen} />
         <Tab.Screen name="AboutUs" component={AboutUsScreen} />
         <Tab.Screen name="ContactUs" component={ContactUsScreen} />
-        <Tab.Screen name="MyProfile" component={MyProfileScreen} />
+        <Tab.Screen options={{headerShown: false}} name="Profile" component={ProfileStack} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ visitorMobile/src/pages/ProfileScreen/Login/Login.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import { NavigationProp, ParamListBase } from '@react-navigation/native';
+import styles from './Login.styles';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {loginUser} from "../../../services/userCalls";
+
+type LoginScreenProps = {
+  navigation: NavigationProp<ParamListBase>;
+};
+
+const LoginScreen: React.FC<LoginScreenProps & { setLoggedInStatus: (status: boolean) => void }> = ({ navigation, setLoggedInStatus }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorForm, setErrorForm] = useState(false);
+
+  const handleSubmit = async () => {
+    try {
+      const dataStorage = JSON.stringify({
+        username: username,
+        password: password
+      });
+
+      const response = await loginUser(dataStorage);
+
+      if (response.data === 'Invalid Access') {
+        setErrorForm(true);
+        AsyncStorage.removeItem('userToken');
+      } else {
+        setErrorForm(false);
+        AsyncStorage.setItem('userToken', response.data);
+        setLoggedInStatus(true);
+        navigation.navigate('Scanning');
+      }
+    } catch (error) {
+      console.error(`Error in Post Route: ${error}`);
+      throw error;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username or Email"
+          value={username}
+          onChangeText={text => setUsername(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={text => setPassword(text)}
+        />
+        {errorForm && <Text style={styles.errorText}>Invalid Logindata</Text>}
+        <TouchableOpacity style={styles.loginButton} onPress={handleSubmit}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableOpacity>
+        <Text style={styles.registerInfo}>
+          Don't you have an account yet? Register yourself{' '}
+          <Text style={styles.registerLink} onPress={() => navigation.navigate('Register')}>
+            here
+          </Text>
+          .
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
