a1a758c7a2a65c4a74993ae6bf3e99078f255ebf
Dubois Renan <renan.dubois@epitech.eu>
2024-02-14 11:00:36 +0900

    [MOBILE] [WIP] Basic Restaurant Screen / Need Filter & Map

--- visitorMobile/src/pages/RestaurantScreen/RestaurantScreen.tsx
+++ visitorMobile/src/pages/RestaurantScreen/RestaurantScreen.tsx
@@ -1,11 +1,71 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState , useCallback} from 'react';
+import { View, FlatList, TouchableOpacity, Text , RefreshControl} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Card from '../../components/RestaurantCard';
+import styles from './RestaurantScreen.styles'
+import axios from 'axios';
+import { getAllResto } from '../../services/restoCalls';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const MyRestaurantsScreen: React.FC = () => {
+export interface IRestaurantFrontEnd {
+  name: string;
+  id: number;
+  phoneNumber: string;
+  website: string;
+  description: string;
+  pictures: string[];
+  hitRate?: number;
+  range: number;
+  rating: number;
+  ratingCount?: number;
+}
+
+
+const MyRestaurantsScreen = () => {
+  const navigation = useNavigation();
+  const [restoData, setRestoData] = useState<IRestaurantFrontEnd[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    updateRestoData();
+  }, []);
+
+  const updateRestoData = async () => {
+    const userToken = await AsyncStorage.getItem('userToken');
+    getAllResto()
+      .then((res) => {
+        setRestoData(res);
+      })
+      .catch((error) => {
+        console.error('Error updating restaurant data:', error);
+      });
+  };
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    updateRestoData();
+    setRefreshing(false);
+  }, []);
+
+
+  const navigateToMenu = (restaurantId: number, restaurantName: string) => {
+    navigation.navigate('MenuPage', { restaurantId, restaurantName });
+  };
+
   return (
-    <View>
-      <Text>My Restaurants</Text>
-      {/* Add any content or functionality for this screen */}
+    <View style={styles.container}>
+      <FlatList
+        data={restoData}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigateToMenu(item.id, item.name)}>
+            <Card info={item}/>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(restaurant) => restaurant.id.toString()}
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
     </View>
   );
 };
