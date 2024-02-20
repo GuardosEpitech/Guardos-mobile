bfaed5b06bf3dfcceb823a904c8d468586cca085
Ramon W <werner.ramon1@googlemail.com>
2024-02-20 17:22:21 +0900

    [FE] [MODIFY] styling about us

--- visitorMobile/src/pages/AboutUs/AboutUs.styles.tsx
+++ visitorMobile/src/pages/AboutUs/AboutUs.styles.tsx
@@ -9,9 +9,10 @@ const styles = StyleSheet.create({
         marginBottom: 20,
       },
       heading: {
-        fontSize: 20,
+        fontSize: 24,
         fontWeight: 'bold',
-        marginBottom: 10,
+        marginBottom: 20,
+        textDecorationLine: 'underline',
       },
       value: {
         marginBottom: 10,
@@ -19,20 +20,70 @@ const styles = StyleSheet.create({
       valueHeading: {
         fontWeight: 'bold',
         marginBottom: 5,
+        fontSize: 20,
       },
       member: {
-        flexDirection: 'row',
         alignItems: 'center',
         marginBottom: 5,
       },
       photo: {
-        width: 50,
-        height: 50,
+        width: 100,
+        height: 100,
         borderRadius: 25,
-        marginRight: 10,
+        marginTop: 10,
+        marginBottom: 5,
+        marginHorizontal: 5,
+
       },
       centerText: {
         textAlign: 'center',
+      },
+      textSize: {
+        fontSize: 18,
+      },
+      separator: {
+        borderBottomWidth: 1,
+        borderBottomColor: 'black',
+        marginBottom: 10,
+        marginTop: 20,
+      },
+      memberText: {
+        marginTop: 5,
+        marginBottom: 20,
+      },
+      teamContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+      },
+      modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      },
+      memberDetails: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+      },
+      modalPhoto: {
+        width: 200,
+        height: 200,
+        borderRadius: 50,
+        marginBottom: 10,
+      },
+      closeButton: {
+        position: 'absolute',
+        bottom: 50,
+        backgroundColor: '#6d071a',
+        padding: 10,
+        borderRadius: 5,
+      },
+      closeButtonText: {
+        color: 'white',
+        fontWeight: 'bold',
       },
 });
 
