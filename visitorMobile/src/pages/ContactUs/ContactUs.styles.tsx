00fd76c933dc05d02eb494fdbdb5825f4aba4083
Ramon W <71701275+wernerramon@users.noreply.github.com>
2024-02-26 07:05:57 +0900

    [FE] [ADD] Eip 229 visitor app contact us (#12)
    * [FE] [ADD] first draft of contact us
    
    * [FE] [MODIFY] styling of the page
    
    * [FE] [MODIFY] moved api call to service folder
    
    ---------
    
    Co-authored-by: Ramon W <werner.ramon1@googlemail.com>

---
+++ visitorMobile/src/pages/ContactUs/ContactUs.styles.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+      },
+      contactInfo: {
+        paddingBottom: 20, // Adjust this value according to your preference
+      },
+      contactForm: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderTopWidth: 1,
+        borderTopColor: '#ccc',
+      },
+      input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+      },
+      button: {
+        backgroundColor: '#6d071a',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        marginBottom: 10,
+      },
+      buttonText: {
+        color: 'white',
+        fontSize: 16,
+      },
+      message: {
+        textAlign: 'center',
+        marginTop: 10,
+      },
+      heading: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        marginBottom: 20,
+        alignSelf: 'center',
+      },
+      contactDetail: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+      },
+      contactText: {
+        marginLeft: 10,
+      },
+});
+
+export default styles;
--- visitorMobile/src/pages/ContactUs/ContactUs.tsx
+++ visitorMobile/src/pages/ContactUs/ContactUs.tsx
@@ -1,13 +1,124 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import styles from './ContactUs.styles';
+import { Ionicons } from '@expo/vector-icons';
+import { IContactForm } from '../../models/emailInterfaces';
+import { sendEmail } from '../../services/emailCalls';
 
 const ContactUs: React.FC = () => {
+  const [formData, setFormData] = useState<IContactForm>({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
+  const handleSubmit = async () => {
+    const { name, email, subject, message } = formData;
+    if (name && email && subject && message && isValidEmail(email)) {
+      try {
+        const response = await sendEmail(formData);
+
+        if (response) {
+          Keyboard.dismiss();
+          setShowConfirmation(true);
+          setFormData({
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+          });
+
+          setTimeout(() => {
+            setShowConfirmation(false);
+          }, 3000);
+        } else {
+          console.error('Backend responded with an error:', response);
+        }
+      } catch (error) {
+        console.error('Error sending data to the backend:', error);
+      }
+    }
+  };
+
+  const isValidEmail = (email: string) => {
+    // Add your email validation logic here
+    return true;
+  };
+
+  const handleChange = (key: keyof IContactForm, value: string) => {
+    setFormData({ ...formData, [key]: value });
+  };
+
   return (
-    <View>
-      <Text>Contact Us</Text>
-  {/* Add any content or functionality for this screen */}
-  </View>
-);
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : undefined} keyboardVerticalOffset={100}>
+        {/* Display contact information */}
+        <Text style={styles.heading}>Get in touch</Text>
+        <View style={styles.contactInfo}>
+          <View style={styles.contactDetail}>
+            <Ionicons name="md-call" size={24} color="black" />
+            <Text style={styles.contactText}>030 1234567</Text>
+          </View>
+
+          <View style={styles.contactDetail}>
+            <Ionicons name="md-mail" size={24} color="black" />
+            <Text style={styles.contactText}>guardos-help@outlook.com</Text>
+          </View>
+
+          <View style={styles.contactDetail}>
+            <Ionicons name="md-pin" size={24} color="black" />
+            <Text style={styles.contactText}>Fasanenstraße 86, 10623 Berlin, Germany</Text>
+          </View>
+        </View>
+
+        {/* Display contact form */}
+        <View style={styles.contactForm}>
+          <Text style={styles.heading}>Contact Form</Text>
+          <TextInput
+            value={formData.name}
+            onChangeText={(text) => handleChange('name', text)}
+            placeholder="Name"
+            style={styles.input}
+          />
+          <TextInput
+            value={formData.email}
+            onChangeText={(text) => handleChange('email', text)}
+            placeholder="Email"
+            style={styles.input}
+          />
+          <TextInput
+            value={formData.subject}
+            onChangeText={(text) => handleChange('subject', text)}
+            placeholder="Subject"
+            style={styles.input}
+          />
+          <TextInput
+            value={formData.message}
+            onChangeText={(text) => handleChange('message', text)}
+            placeholder="Message"
+            multiline
+            style={[styles.input, { height: 100 }]}
+          />
+
+          {/* Submit button */}
+          <TouchableOpacity onPress={handleSubmit} style={[styles.button, { backgroundColor: '#6d071a' }]}>
+            <Text style={styles.buttonText}>Submit</Text>
+          </TouchableOpacity>
+
+          {/* Confirmation message */}
+          {showConfirmation && (
+            <Text style={[styles.message, { color: 'green' }]}>Message sent successfully!</Text>
+          )}
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
+  );
 };
 
 export default ContactUs;
