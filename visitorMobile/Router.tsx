80f591cd47235abfea72a258fae0fc481a6d7959
MarcPIster <marc.pister@epitech.eu>
2024-06-24 15:05:13 +0200

    [FE] [ADD] add burger menu for visitor app

--- visitorMobile/Router.tsx
+++ visitorMobile/Router.tsx
@@ -1,77 +1,168 @@
-import React, {useState, createContext, useEffect,  } from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {LogBox} from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Platform, NativeModules, View, Text, StyleSheet } from 'react-native';
+import NetInfo from "@react-native-community/netinfo";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { useTranslation } from "react-i18next";
+import './i18n/i18n';
 
+// Import your screens and components
 import RestaurantScreen from './src/pages/RestaurantScreen/RestaurantScreen';
 import AboutUsScreen from './src/pages/AboutUs/AboutUs';
 import ContactUsScreen from './src/pages/ContactUs/ContactUs';
 import MapPage from './src/pages/MapPage/MapPage';
 import Register from "./src/pages/ProfileScreen/Register/Register";
 import LoginScreen from "./src/pages/ProfileScreen/Login/Login";
-// import {checkIfTokenIsValid} from "./src/services/userCalls";
-// import MyProfileScreen from './src/pages/Profile/MyProfile';
 import MenuPage from './src/pages/MenuPage/MenuPage';
 import ResetPassword from './src/pages/ResetPasswordScreen/ResetPasswordScreen';
 import ChangePasswordScreen from "./src/pages/ProfileScreen/ChangePassword/ChangePassword";
 import Profile from "./src/pages/ProfileScreen/Profile/Profile";
 import FeatureRequest from './src/pages/FeatureRequest/FeatureRequest';
-import { ISearchCommunication } from '../shared/models/communicationInterfaces';
-import { FilterContext } from './src/models/filterContext';
-import {useTranslation} from "react-i18next";
 import PrivacyPage from './src/pages/PrivacyPage/PrivacyPage';
 import ImprintPage from './src/pages/ImprintPage/ImprintPage';
 import AppIntro from './src/pages/AppIntro/AppIntro';
 import SubscriptionPage from "./src/pages/SubscriptionPage/SubscriptionPage";
 import PaymentPage from './src/pages/Payment/PaymentPage';
+import i18n from "i18next";
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
 
-LogBox.ignoreLogs(['Warning: ...']);
-LogBox.ignoreAllLogs()
+const ErrorScreen: React.FC<{ errorMessage: string }> = ({ errorMessage }) => (
+    <View style={styles.container}>
+      <Text style={styles.errorText}>{errorMessage}</Text>
+    </View>
+);
 
-const MyTabs = () => {
+const MainDrawer = ({ setLoggedInStatus }) => {
+  const { t } = useTranslation();
+
+  return (
+      <Drawer.Navigator
+          screenOptions={({ route }) => ({
+            drawerIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              switch (route.name) {
+                case 'RestaurantScreen':
+                  iconName = focused ? 'restaurant' : 'restaurant-outline';
+                  break;
+                case 'MapScreen':
+                  iconName = focused ? 'map' : 'map-outline';
+                  break;
+                case 'AboutUs':
+                  iconName = focused ? 'information-circle' : 'information-circle-outline';
+                  break;
+                case 'ContactUs':
+                  iconName = focused ? 'call' : 'call-outline';
+                  break;
+                case 'My Profile':
+                  iconName = focused ? 'person' : 'person-outline';
+                  break;
+                default:
+                  iconName = 'alert-circle-outline'; // Default icon for unrecognized routes
+                  break;
+              }
+
+              return <Ionicons name={iconName} size={size} color={focused ? 'black' : color} />;
+            },
+            drawerActiveTintColor: 'black',
+            drawerInactiveTintColor: 'black',
+          })}
+      >
+        <Drawer.Screen
+            name="RestaurantScreen"
+            options={{
+              drawerLabel: t('pages.Router.resto-screen') as string,
+              title: t('pages.Router.resto-screen') as string,
+              headerShown: true,
+              headerStyle: { backgroundColor: '#6d071a' },
+            }}
+            component={RestaurantScreen}
+        />
+        <Drawer.Screen
+            name="MapScreen"
+            component={MapPage}
+            options={{
+              drawerLabel: t('pages.Router.map-screen') as string,
+              title: t('pages.Router.map-screen') as string,
+              headerShown: true,
+              headerStyle: { backgroundColor: '#6d071a' },
+            }}
+        />
+        <Drawer.Screen
+            name="AboutUs"
+            component={AboutUsScreen}
+            options={{
+              drawerLabel: t('pages.Router.about-us') as string,
+              title: t('pages.Router.about-us') as string,
+              headerShown: true,
+              headerStyle: { backgroundColor: '#6d071a' },
+            }}
+        />
+        <Drawer.Screen
+            name="ContactUs"
+            component={ContactUsScreen}
+            options={{
+              drawerLabel: t('pages.Router.contact-us') as string,
+              title: t('pages.Router.contact-us') as string,
+              headerShown: true,
+              headerStyle: { backgroundColor: '#6d071a' },
+            }}
+        />
+        <Drawer.Screen
+            name="My Profile"
+            options={{ headerShown: true, headerStyle: { backgroundColor: '#6d071a' } }}
+        >
+          {() => <ProfileStackScreen setLoggedInStatus={setLoggedInStatus} />}
+        </Drawer.Screen>
+      </Drawer.Navigator>
+  );
+};
+
+const Router: React.FC = () => {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [filter, setFilter] = useState<ISearchCommunication>({});
   const [showIntro, setShowIntro] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
-  const {t} = useTranslation();
-
-  const setLoggedInStatus = (status) => {
-    setLoggedIn(status);
-  };
+  const [hasInternetConnection, setHasInternetConnection] = useState(true);
+  const { t } = useTranslation();
 
   useEffect(() => {
-    fetchIntro();
-    fetchDarkMode();    
-  });
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setHasInternetConnection(state.isConnected);
+    });
+
+    fetchUserLanguage();
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const fetchUserLanguage = async () => {
+    console.log('Fetching user language...');
+    const deviceLanguage =
+        (Platform.OS === 'ios'
+            ? NativeModules.SettingsManager.settings.AppleLocale ||
+            NativeModules.SettingsManager.settings.AppleLanguages[0] // iOS 13
+            : NativeModules.I18nManager.localeIdentifier).split('_')[0];
+
+    if (deviceLanguage === 'en' || deviceLanguage === 'de' || deviceLanguage === 'fr') {
+      console.log('Setting user language to:', deviceLanguage);
+      i18n.changeLanguage(deviceLanguage);
+    }
+  };
 
   const fetchIntro = async () => {
     try {
       const introValue = await AsyncStorage.getItem('introShown');
       if (introValue === null) {
         setShowIntro(true);
-      }/* else {
-       setShowIntro(true);
-      } Use this to force the intro screen to show */
-    } catch (error) {
-      console.error('Error fetching intro value:', error);
-    }
-  };
-
-  const fetchDarkMode = async () => {
-    try {
-      const darkModeValue = await AsyncStorage.getItem('DarkMode');
-      if (darkModeValue !== null) {
-        const isDarkMode = darkModeValue === 'true';
-        setDarkMode(isDarkMode);
       }
     } catch (error) {
-      console.error('Error fetching dark mode value:', error);
+      console.error('Error fetching intro value:', error);
     }
   };
 
@@ -80,225 +171,98 @@ const MyTabs = () => {
     AsyncStorage.setItem('introShown', 'true');
   };
 
-  // const checkAuthentication = async () => {
-  //   try {
-  //     const userToken = await AsyncStorage.getItem('userToken');
-  //
-  //     if (userToken === null) {
-  //       setLoggedIn(false);
-  //       return;
-  //     }
-  //
-  //     const isUserTokenValid = await checkIfTokenIsValid({ key: userToken });
-  //
-  //     if (isUserTokenValid === 'OK') {
-  //       setLoggedIn(true);
-  //     } else {
-  //       setLoggedIn(false);
-  //       await AsyncStorage.removeItem('userToken');
-  //     }
-  //   } catch (error) {
-  //     console.error('Error fetching login data:', error);
-  //   }
-  // };
-  //
-  // useEffect(() => {
-  //   checkAuthentication();
-  // }, []);
+  useEffect(() => {
+    fetchIntro();
+  }, []);
 
-  return (
-    <FilterContext.Provider value={{ filter, setFilter }}>
-      <NavigationContainer> 
-        {loggedIn && showIntro ? (
-        <AppIntro onFinish={handleIntroFinish} />
-      ) : (
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-  
-              if (route.name === 'RestaurantScreen') {
-                iconName = focused ? 'restaurant' : 'restaurant-outline';
-              } else if (route.name === 'MapScreen') {
-                iconName = focused ? 'map' : 'map-outline';
-              } else if (route.name === 'AboutUs') {
-                iconName = focused ? 'information-circle' : 'information-circle-outline';
-              } else if (route.name === 'ContactUs') {
-                iconName = focused ? 'call' : 'call-outline';
-              } else if (route.name === 'My Profile') {
-                iconName = focused ? 'person' : 'person-outline';
-              } else if (route.name === 'Login') {
-                iconName = focused ? 'log-in' : 'log-in-outline';
-              } else if (route.name === 'Register') {
-                iconName = focused ? 'person-add' : 'person-add-outline';
-              } else if (route.name === 'MenuPage') {
-                return null;
-              } else if (route.name === 'Account Recovery') {
-                iconName = focused ? 'settings' : 'settings-outline';
-              }
-  
-              return <Ionicons name={iconName} size={size} color={focused ? 'black' : color} />;
-            },
-            tabBarActiveTintColor: 'black',
-            tabBarInactiveTintColor: 'black',
-            tabBarStyle: {
-              backgroundColor: '#6d071a',
-            },
-          })}
-        >
-          {loggedIn ? (
-            <>
-              <Tab.Screen
-                name="RestaurantScreen"
-                options={{
-                  tabBarLabel: t('pages.Router.resto-screen') as string,
-                  title: t('pages.Router.resto-screen') as string,
-                  headerShown: true,
-                  headerStyle: { backgroundColor: '#6d071a' }
-                }}
-                component={RestauStack}
-              />
-              <Tab.Screen
-                name="MapScreen"
-                component={MapPage}
-                options={{
-                  tabBarLabel: t('pages.Router.map-screen') as string,
-                  title: t('pages.Router.map-screen') as string,
-                  headerShown: true,
-                  headerStyle: { backgroundColor: '#6d071a' }
-                }}
-              />
-              <Tab.Screen
-                name="AboutUs"
-                component={AboutUsScreen}
-                options={{
-                  tabBarLabel: t('pages.Router.about-us') as string,
-                  title: t('pages.Router.about-us') as string,
-                  headerShown: true,
-                  headerStyle: { backgroundColor: '#6d071a' }
-                }}
-              />
-              <Tab.Screen
-                name="ContactUs"
-                component={ContactUsScreen}
-                options={{
-                  tabBarLabel: t('pages.Router.contact-us') as string,
-                  title: t('pages.Router.contact-us') as string,
-                  headerShown: true,
-                  headerStyle: { backgroundColor: '#6d071a' }
-                }}
-              />
-              <Tab.Screen
-                name="My Profile"
-                options={{ headerShown: true, headerStyle: { backgroundColor: '#6d071a' } }}>
-                {() => <ProfileStackScreen setLoggedInStatus={setLoggedInStatus} />}
-              </Tab.Screen>
-              <Tab.Screen
-                name="MenuPage"
-                component={MenuPage}
-                options={{
-                  headerShown: true,
-                  headerStyle: { backgroundColor: '#6d071a' },
-                  tabBarButton: () => null
-                }}
-              />
-            </>
+  const renderContent = () => {
+    if (!hasInternetConnection) {
+      return <ErrorScreen errorMessage={t('pages.Router.no-internet-error') as string} />;
+    }
+    return (
+        <NavigationContainer>
+          {loggedIn && showIntro ? (
+              <AppIntro onFinish={handleIntroFinish} />
           ) : (
-            <>
-              <Tab.Screen
-                name="Login"
-                options={{
-                  headerShown: false,
-                  tabBarLabel: t('pages.Router.login') as string,
-                  title: t('pages.Router.login') as string
-                }}
-              >
-                {() => <LoginStackScreen setLoggedInStatus={setLoggedInStatus} />}
-              </Tab.Screen>
-              <Tab.Screen
-                name="Register"
-                component={Register}
-                options={{
-                  tabBarLabel: t('pages.Router.register') as string,
-                  title: t('pages.Router.register') as string
-                }}
-              />
-            </>
+              <Stack.Navigator>
+                {!loggedIn ? (
+                    <>
+                      <Stack.Screen name="Login" options={{ headerShown: false }}>
+                        {(props) => <LoginScreen {...props} setLoggedInStatus={setLoggedIn} />}
+                      </Stack.Screen>
+                      <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+                    </>
+                ) : (
+                    <Stack.Screen name="Main" options={{ headerShown: false }}>
+                      {(props) => <MainDrawer {...props} setLoggedInStatus={setLoggedIn} />}
+                    </Stack.Screen>
+                )}
+              </Stack.Navigator>
           )}
-        </Tab.Navigator>
-      )}
-      </NavigationContainer>
-    </FilterContext.Provider>
-  );
+        </NavigationContainer>
+    );
+  };
+
+  return renderContent();
 };
 
 interface ProfileStackProps {
   setLoggedInStatus: (status: boolean) => void;
 }
 
-const ProfileStackScreen: React.FC<ProfileStackProps> = ({setLoggedInStatus}) => {
-  const {t} = useTranslation();
+const ProfileStackScreen: React.FC<ProfileStackProps> = ({ setLoggedInStatus }) => {
+  const { t } = useTranslation();
 
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Profile"
-        options={{
-          tabBarLabel: t('pages.Router.my-profile') as string,
-          title: t('pages.Router.my-profile') as string,
-          headerShown: false,
-        }}
-      >
-        {(props) => <Profile {...props} setLoggedInStatus={setLoggedInStatus}/>}
-      </Stack.Screen>
-      <Stack.Screen name="FeatureRequest" component={FeatureRequest}  options={{ headerShown: true, headerStyle: {backgroundColor: '#6d071a'}}}/>
-      <Stack.Screen name="Subscriptions" component={SubscriptionPage}  options={{ headerShown: true, headerStyle: {backgroundColor: '#6d071a'}}}/>
-      <Stack.Screen name="Change Password" component={ChangePasswordScreen}/>
-      <Stack.Screen name="Privacy" component={PrivacyPage} options={{ headerShown: true, headerStyle: {backgroundColor: '#6d071a'}}}/>
-      <Stack.Screen name="Imprint" component={ImprintPage} options={{ headerShown: true, headerStyle: {backgroundColor: '#6d071a'}}}/>
-      <Stack.Screen name="Payment methods" component={PaymentPage} options={{ headerShown: true, headerStyle: {backgroundColor: '#6d071a'}}}/>
-    </Stack.Navigator>
-  );
-}
-
-const RestauStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="RestaurantScreen"
-        component={RestaurantScreen}
-        options={{headerShown: false, headerStyle: {backgroundColor: 'black'}}}
-      />
-      <Stack.Screen
-        name="MenuPage"
-        component={MenuPage}
-        options={{headerShown: false, headerStyle: {backgroundColor: '#6d071a'}}}
-      />
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen
+            name="Profile"
+            options={{
+              title: t('pages.Router.my-profile') as string,
+              headerShown: false,
+            }}
+        >
+          {(props) => <Profile {...props} setLoggedInStatus={setLoggedInStatus} />}
+        </Stack.Screen>
+        <Stack.Screen name="FeatureRequest" component={FeatureRequest} options={{ headerShown: true, headerStyle: { backgroundColor: '#6d071a' } }} />
+        <Stack.Screen name="Subscriptions" component={SubscriptionPage} options={{ headerShown: true, headerStyle: { backgroundColor: '#6d071a' } }} />
+        <Stack.Screen name="Change Password" component={ChangePasswordScreen} />
+        <Stack.Screen name="Privacy" component={PrivacyPage} options={{ headerShown: true, headerStyle: { backgroundColor: '#6d071a' } }} />
+        <Stack.Screen name="Imprint" component={ImprintPage} options={{ headerShown: true, headerStyle: { backgroundColor: '#6d071a' } }} />
+        <Stack.Screen name="Payment methods" component={PaymentPage} options={{ headerShown: true, headerStyle: { backgroundColor: '#6d071a' } }} />
+      </Stack.Navigator>
   );
 };
 
-interface LoginStackProps {
-  setLoggedInStatus: (status: boolean) => void;
-}
-
-const LoginStackScreen: React.FC<LoginStackProps> = ({ setLoggedInStatus }) => {
-  const {t} = useTranslation();
-
+const RestauStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Login"
-        options={{
-          tabBarLabel: t('pages.Router.login') as string,
-          title: t('pages.Router.login') as string
-        }}
-      >
-        {(props) => <LoginScreen {...props} setLoggedInStatus={setLoggedInStatus} />}
-      </Stack.Screen>
-      <Stack.Screen name="Account Recovery" component={ResetPassword} />
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen
+            name="RestaurantScreen"
+            component={RestaurantScreen}
+            options={{ headerShown: false }}
+        />
+        <Stack.Screen
+            name="MenuPage"
+            component={MenuPage}
+            options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
   );
-}
-  
-export default MyTabs;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+});
+
+export default Router;
