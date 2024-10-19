1f088ea8e9412ca71c90fbc185fb460c0a6af803
Ramon W <ramon.werner@epitech.eu>
2024-10-19 22:26:23 +0200

    [FE] [FIX] removed fake phone number

--- visitorMobile/src/pages/PrivacyPage/PrivacyPage.tsx
+++ visitorMobile/src/pages/PrivacyPage/PrivacyPage.tsx
@@ -135,7 +135,6 @@ const PrivacyPage = () => {
               <Text />
               <Text style={[styles.text, darkMode && styles.textDarkTheme]}>{t('pages.Privacy.address')}</Text>
               <Text style={[styles.text, darkMode && styles.textDarkTheme]}>{t('pages.Privacy.email')}</Text>
-              <Text style={[styles.text, darkMode && styles.textDarkTheme]}>{t('pages.Privacy.phone')}</Text>
               <Text />
               <Text style={[styles.text, darkMode && styles.textDarkTheme]}>{t('pages.Privacy.end')}</Text>
               <Text />
