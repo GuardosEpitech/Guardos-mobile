832470ee4b153760dd11af182df8c113c78e29c9
Dubois Renan <renan.dubois@epitech.eu>
2024-11-09 13:29:39 +0100

    [APP] [FIX] Fix styling for the light mode

--- visitorMobile/src/components/RestaurantCard/RestaurantCard.styles.ts
+++ visitorMobile/src/components/RestaurantCard/RestaurantCard.styles.ts
@@ -172,6 +172,8 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
+    alignItems: 'center',
+    flexDirection: 'column',
   },
   modalTitle: {
     fontSize: 20,
