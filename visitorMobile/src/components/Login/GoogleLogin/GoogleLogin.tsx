9b8fa4d3dd38c945eb6a6ef1001ec618810e42cf
MarcPIster <marc.pister@epitech.eu>
2024-11-11 11:36:00 +0100

    [APP][DEVOPS] try something

--- visitorMobile/src/components/Login/GoogleLogin/GoogleLogin.tsx
+++ visitorMobile/src/components/Login/GoogleLogin/GoogleLogin.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
-import {Button, View} from 'react-native';
+import { View } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useTranslation } from 'react-i18next';
 import { getVisitorProfileDetails } from '../../../services/profileCalls';
 import {styles} from "./styles";
-import * as AuthSession from 'expo-auth-session';
-import * as Google from 'expo-auth-session/providers/google';
+import {GoogleSignin, GoogleSigninButton} from "@react-native-google-signin/google-signin";
 import {loginUser} from "../../../services/userCalls";
 
 const GOOGLE_CLIENT_ID = '251768771069-7v10qin71ssbe5au77dt6at41s6cgglg.apps.googleusercontent.com';
@@ -19,21 +19,29 @@ type GoogleLoginButtonProps = {
 const GoogleLoginButton: React.FC<GoogleLoginButtonProps> = ({ setLoggedInStatus, navigation }) => {
   const { t, i18n } = useTranslation();
   const [error, setError] = React.useState<string | null>(null);
+  const configureGoogleSignIn = () => {
+    GoogleSignin.configure({
+      webClientId: WEB_CLIENT_ID,
+      androidClientId: GOOGLE_CLIENT_ID,
+      iosClientId: GOOGLE_CLIENT_ID,
+    });
+  };
 
-  const redirectUri = 'https://auth.expo.dev/@marcpister/visitorMobile';
-  console.log('Redirect URI:', redirectUri);
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: WEB_CLIENT_ID,
-    redirectUri: redirectUri,
-  });
+  const signIn = async () => {
+    try {
+      console.log('Google Sign-In button pressed');
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      await handleGoogleLogin(userInfo);
+    } catch (error) {
+      console.error('Google Sign-In error:', error);
+      setError(error);
+    }
+  }
 
   useEffect(() => {
-    if (response?.type === 'success') {
-      const { authentication } = response;
-      console.log('Google Auth Success:', authentication);
-      handleGoogleLogin(authentication).then();
-    }
-  }, [response]);
+    configureGoogleSignIn();
+  });
 
   const handleGoogleLogin = async (userInfo: any) => {
     try {
@@ -43,18 +51,18 @@ const GoogleLoginButton: React.FC<GoogleLoginButtonProps> = ({ setLoggedInStatus
       });
       console.log('Data storage:', dataStorage)
       const response = await loginUser(dataStorage);
-      if (response === 'Invalid Access') {
+      if (response.data === 'Invalid Access') {
         AsyncStorage.removeItem('userToken');
         AsyncStorage.removeItem('userName');
       } else {
-        getVisitorProfileDetails(response)
+        getVisitorProfileDetails(response.data)
             .then((res) => {
               if (res.preferredLanguage) {
                 i18n.changeLanguage(res.preferredLanguage);
               }
             });
         await AsyncStorage.setItem('userToken', JSON.stringify('isSet'));
-        await AsyncStorage.setItem('user', response);
+        await AsyncStorage.setItem('user', response.data);
         setLoggedInStatus(true);
         navigation.navigate('RestaurantScreen');
       }
@@ -65,7 +73,12 @@ const GoogleLoginButton: React.FC<GoogleLoginButtonProps> = ({ setLoggedInStatus
 
   return (
       <View>
-        <Button title="Sign in with Google" onPress={() => promptAsync()} />
+        <GoogleSigninButton
+            style={styles.googleButton}
+            size={GoogleSigninButton.Size.Icon}
+            color={GoogleSigninButton.Color.Light}
+            onPress={signIn}
+        />
       </View>
   );
 };
