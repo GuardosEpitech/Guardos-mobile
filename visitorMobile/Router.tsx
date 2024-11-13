d9137bfd038708a78d845ce8067556d5886e5b91
Ramon W <ramon.werner@epitech.eu>
2024-11-13 13:14:34 +0100

    [FE] [FIX] fix same filter map and list + apply user allergens directly

--- visitorMobile/Router.tsx
+++ visitorMobile/Router.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -30,6 +30,8 @@ import MenuPage from './src/pages/MenuPage/MenuPage';
 import i18n from "i18next";
 import GuidesPage from "./src/pages/Guides/GuidesPage";
 import ResetPassword from './src/pages/ResetPasswordScreen/ResetPasswordScreen';
+import { ISearchCommunication } from '../shared/models/communicationInterfaces';
+import { FilterContext } from './src/models/filterContext';
 
 const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
@@ -44,6 +46,7 @@ const ErrorScreen: React.FC<{ errorMessage: string }> = ({ errorMessage }) => (
 const MainDrawer = ({ setLoggedInStatus }) => {
   const { t } = useTranslation();
   const [darkMode, setDarkMode] = useState<boolean>(false);
+  const [filter, setFilter] = useState<ISearchCommunication>({});
 
   useEffect(() => {
     const fetchDarkMode = async () => {
@@ -63,6 +66,7 @@ const MainDrawer = ({ setLoggedInStatus }) => {
 
 
   return (
+    <FilterContext.Provider value={{ filter, setFilter }}>
       <Drawer.Navigator
           screenOptions={({ route }) => ({
             drawerIcon: ({ focused, color, size }) => {
@@ -158,6 +162,7 @@ const MainDrawer = ({ setLoggedInStatus }) => {
           {() => <ProfileStackScreen setLoggedInStatus={setLoggedInStatus} />}
         </Drawer.Screen>
       </Drawer.Navigator>
+      </FilterContext.Provider>
   );
 };
 
