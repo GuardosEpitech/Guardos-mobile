1bcfa5a26bb2f2a02ffd6e7cfc1815030d452420
Alban de Tourtier <71947647+Alban-de-tourtier@users.noreply.github.com>
2024-11-13 10:01:06 +0100

    Back log alban nov (#93)
    * add qr code menu
    
    * add qr code on created
    
    * modify featurerequest required fields
    
    * modify featurerequest required fields
    
    * modify featurerequest required fields
    
    * modify error manage payement

--- restoMobile/src/pages/QRCodeEngin/QRCodeEngin.tsx
+++ restoMobile/src/pages/QRCodeEngin/QRCodeEngin.tsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button, Pressable } from "react-native";
+import {Text, View, StyleSheet, Button, Pressable, ScrollView, Alert} from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import styles from "./QRCodeEngin.styles";
 import axios from "axios";
-import { API_URL } from '@env';
 import { Dropdown } from 'react-native-element-dropdown';
-import { Restaurant } from "src/models/ingedientsInterfaces";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {useTranslation} from "react-i18next";
+import {getAllRestaurantsByUserAndFilter} from "../../services/restoCalls";
+import {IRestaurantFrontEnd} from "../../models/restaurantsInterfaces";
+import {IProduct} from "../../../../shared/models/restaurantInterfaces";
+import {addNewProduct, editProduct} from "../../services/productCalls";
+import {IProductFE} from "../../../../shared/models/productInterfaces";
 
 const QRCodeEngin = ({ navigation }: { navigation: any }) => {
   const [hasPermission, setHasPermission] = useState(null);
@@ -15,9 +18,11 @@ const QRCodeEngin = ({ navigation }: { navigation: any }) => {
   const [data, setData] = useState("");
   const [value, setValue] = useState(null);
   const [productName, setProductName] = useState("");
-  const [RestoValue, setRestoValue] = useState<{ label: string; value: string }[]>([]);
   const [darkMode, setDarkMode] = useState<boolean>(false);
+  const [fieldRequire, setFieldRequire] = useState<boolean>();
   const {t} = useTranslation();
+  const [filter, setFilter] = useState('');
+  const [restoData, setRestoData] = useState<IRestaurantFrontEnd[]>([]);
 
   useEffect(() => {
     (async () => {
@@ -25,8 +30,24 @@ const QRCodeEngin = ({ navigation }: { navigation: any }) => {
       setHasPermission(status === "granted");
     })();
     fetchDarkMode();
-    getResto();
+    getRestoData(filter);
   }, []);
+
+  const getRestoData = async (filter: string) => {
+    const userToken = await AsyncStorage.getItem('userToken');
+    if (!userToken) return;
+
+    try {
+      const res = await getAllRestaurantsByUserAndFilter(userToken, filter);
+      const newData = res.map((item: any) => ({
+        label: item.name,
+        value: item.name,
+      }));
+      setRestoData(newData);
+    } catch (error) {
+      console.error('Error updating restaurant data:', error);
+    }
+  };
 
   const fetchDarkMode = async () => {
     try {
@@ -62,50 +83,35 @@ const QRCodeEngin = ({ navigation }: { navigation: any }) => {
     return <Text>{t('common.need-cam-permissions')}</Text>;
   }
 
-  function GoToAddPage() {
-    navigation.navigate("AddPage");
-  }
-
-  async function getResto() {
-    try {
-      const response = await axios({
-        method: "get",
-        url: API_URL + "restaurants",
-      });
-  
-      // Assuming response.data is an array of objects with a 'name' property
-      const newData = response.data.map((item: Restaurant) => ({
-        label: item.name,
-        value: item.id, // Replace 'id' with the actual property name for the value
-      }));
-  
-      setRestoValue(newData);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  async function AddProduct() {
-    try {
-      const response = await axios({
-        url: `http://195.90.210.111:8081/api/products/${value}`,
-        method: "POST",
-        data: JSON.stringify({
+  const handleAddProduct = async () => {
+    if (value === null) {
+      setFieldRequire(false);
+    } else {
+      try {
+        const product: IProduct = {
           name: productName,
-          resto: value,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (response.status == 200) GoToAddPage();
-    } catch (err) {
-      console.log(err);
+          allergens: [],
+          ingredients: [],
+        };
+        const userToken = await AsyncStorage.getItem('userToken');
+        if (userToken === null) {
+          return;
+        }
+        await addNewProduct(product, value, userToken);
+        setValue(null)
+        setScanned(false)
+        setFieldRequire(null);
+        navigation.navigate('MyProductsScreen');
+      } catch (error) {
+        console.error('Error adding product:', error);
+      }
     }
-  }
+  };
+
   return (
   <View style={[{ flex: 1 }, darkMode ? styles.containerDarkTheme : styles.containerLightTheme]}>
-    <View style={{marginTop: 5, alignItems: "center"}}>
+    <View style={{marginTop: 5, alignItems: "center" }}>
+    <ScrollView >
       <View style={styles.DivTop}>
         <Text style={styles.TitleIngr}>{t('pages.QRCodeEngin.scan-ingredients')}</Text>
         <Text>{t('pages.QRCodeEngin.scan-prompt')}</Text>
@@ -115,7 +121,7 @@ const QRCodeEngin = ({ navigation }: { navigation: any }) => {
             selectedTextStyle={styles.selectedTextStyle}
             inputSearchStyle={styles.inputSearchStyle}
             iconStyle={styles.iconStyle}
-            data={RestoValue}
+            data={restoData}
             search
             maxHeight={300}
             labelField="label"
@@ -136,6 +142,11 @@ const QRCodeEngin = ({ navigation }: { navigation: any }) => {
             style={StyleSheet.absoluteFillObject}
           />
         </View>
+        { !fieldRequire && scanned && !value ?
+          <Text style={{color: "red", marginTop: 10}}>{t('pages.QRCodeEngin.required-field')}</Text>
+            :
+            <Text></Text>
+        }
         {scanned && (
           <View>
             <Button title={t('pages.QRCodeEngin.scan-again')} onPress={() => setScanned(false)} />
@@ -143,7 +154,7 @@ const QRCodeEngin = ({ navigation }: { navigation: any }) => {
             <Text style={styles.TitleScan}>
               {t('pages.QRCodeEngin.scan-result-ask-if-want-to-add', {data: data, productName: productName})}
             </Text>
-              <View style={styles.DivTop}>
+              <View style={styles.DivBtn}>
                 <Pressable
                   style={styles.ButtonNo}
                   onPress={() => {
@@ -155,8 +166,7 @@ const QRCodeEngin = ({ navigation }: { navigation: any }) => {
                 <Pressable
                   style={styles.ButtonYes}
                   onPress={() => {
-                    console.log("ONpressYES");
-                    AddProduct();
+                    handleAddProduct();
                   }}
                 >
                   <Text style={{ color: "white" }}>{t('common.yes')}</Text>
@@ -166,6 +176,7 @@ const QRCodeEngin = ({ navigation }: { navigation: any }) => {
           </View>
         )}
       </View>
+      </ ScrollView>
     </View>
     </View>
   );
