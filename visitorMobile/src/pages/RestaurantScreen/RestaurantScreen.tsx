ed12cdde665521b610c148a9f63498ef0a0a190a
Ramon W <ramon.werner@epitech.eu>
2024-11-16 12:20:54 +0100

    [FE] [FIX] show ads only to free users

--- visitorMobile/src/pages/RestaurantScreen/RestaurantScreen.tsx
+++ visitorMobile/src/pages/RestaurantScreen/RestaurantScreen.tsx
@@ -38,7 +38,7 @@ import Icon from "react-native-vector-icons/Ionicons";
 import * as Location from 'expo-location';
 import {getUserAllergens} from "../../services/userCalls";
 import {getCategories} from "../../services/categorieCalls";
-
+import { getVisitorUserPermission } from '../../services/permissionsCalls';
 
 const MyRestaurantsScreen = () => {
   const navigation = useNavigation();
@@ -86,6 +86,7 @@ const MyRestaurantsScreen = () => {
   const adFrequency = 5;
   const dataWithAds = [...selectedRestoData];
   const [userPosition, setUserPosition] = React.useState<{ lat: number; lng: number } | null>(null);
+  const [premium, setPremium] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
@@ -153,10 +154,27 @@ const MyRestaurantsScreen = () => {
   
   }, []);
 
+  const getPremium = async () => {
+    try {
+      const userToken = await AsyncStorage.getItem('userToken');
+      const permissions = await getVisitorUserPermission(userToken);
+      const isPremiumUser = permissions.includes('premiumUser');
+      const isBasicUser = permissions.includes('basicSubscription');
+      if (isBasicUser || isPremiumUser) {
+        setPremium(true);
+      } else {
+        setPremium(false);
+      }
+    } catch (error) {
+      console.error('Error getting permissions: ', error);
+    }
+  };
+
   useEffect(() => {
     if (isFocused) {
       loadSavedFilters();
     }
+    getPremium();
     fetchFavourites().then(r => console.log("Loaded favourite resto list"));
     fetchDarkMode();
     fetchGroupProfile();
@@ -794,7 +812,7 @@ const MyRestaurantsScreen = () => {
           <FlatList
           data={dataWithAds}
           renderItem={({ item, index }) => {
-            if (item.isAd) {
+            if (item.isAd && !premium) {
               return <AdCard />;
             } else {
               return (
